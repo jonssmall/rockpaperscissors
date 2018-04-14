4fbe612d0ab20dd93137d8d7e65f5e9d1003d587
Jon Small <jonsmall@jons-air.lan>
2018-04-14 00:10:09 -0500

    resetting game

--- public/game.js
+++ public/game.js
@@ -23,16 +23,26 @@ const moves = {
 };
 
 const player1 = {
-  tag: '#player-1'
+  tag: '#player-1',
+  pictures: {
+    rock: 'rockRight.jpeg',
+    paper: 'paperRight.jpeg',
+    scissors: 'scissorsRight.jpeg'
+  }
 };
 
 const player2 = {
-  tag: '#player-2'
+  tag: '#player-2',
+  pictures: {
+    rock: 'rockLeft.jpeg',
+    paper: 'paperLeft.jpeg',
+    scissors: 'scissorsLeft.jpeg'
+  }
 };
 
 const players = [player1, player2];
 
-function bindMoves(player) {
+function bindMoves(player) {  
   document.querySelectorAll(`${player.tag} button`).forEach((button, i, bArr) => {    
     button.addEventListener('click', function(event) {
       bArr.forEach(b => b.disabled = false);
@@ -48,6 +58,7 @@ function bindMoves(player) {
 players.forEach(p => bindMoves(p));
 
 function getWinner() {  
+  document.querySelector('#game-board').hidden = false;
   if (player1.move.name === player2.move.win) {
     alert('player2 wins.');
   } else if (player1.move.name === player2.move.lose) {
@@ -66,9 +77,20 @@ clientSocket.onopen = event => {
   }, 24000);
 };
 clientSocket.onmessage = event => {      
-  if (event.data !== 'ping') console.log(event.data);
+  if (event.data !== '"ping"') console.log(event.data);
 };
 
 function sendMessage(message) {
   clientSocket.send(message);
+}
+
+document.querySelector('#reset-button').addEventListener('click', function(button) {
+  resetGame();
+});
+
+function resetGame() {
+  delete player1.move;
+  delete player2.move;
+  document.querySelector('#game-board').hidden = true;
+  document.querySelectorAll(`button`).forEach(button => button.disabled = false);    
 }
